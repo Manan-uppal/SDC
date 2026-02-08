352600d942d402e29fa182f15c2a5a21920d128d
Manan_Uppal <muppal004@gmail.com>
2026-02-09 01:03:42 +0530

    records and play audio

---
+++ code/main.py
@@ -0,0 +1,28 @@
+import sounddevice as sd
+import numpy as np 
+
+sd.default.samplerate = 48000
+sd.default.blocksize = 1024
+sd.default.channels=1
+
+rec_list = []
+def audio_callback(indata, frames, time, status):
+    if status :
+        print(status)
+    rec_list.append(indata.copy())
+
+def playingsound(arr, sampling_rate = sd.default.samplerate):
+     sd.play(arr, sampling_rate)
+     sd.wait()
+
+with sd.InputStream(
+    callback=audio_callback
+):
+    input("press enter to stop\n")
+
+
+full_audio = np.concatenate(rec_list, axis=0)
+playingsound(full_audio)
+
+
+
