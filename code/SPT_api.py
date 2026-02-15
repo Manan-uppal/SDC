72290333f2560b442c6a91378fbcf05694b188d3
Manan_Uppal <muppal004@gmail.com>
2026-02-15 17:25:50 +0530

    version_1

---
+++ code/SPT_api.py
@@ -0,0 +1,15 @@
+import assemblyai as aai
+
+aai.settings.api_key = "bf34c4cf41a64aea8782324a682120d0"
+config = aai.TranscriptionConfig(speech_models=["universal-3-pro", "universal-2"], language_detection=True)
+
+
+def convertor(location):
+    
+    transcript = aai.Transcriber(config=config).transcribe(location)
+    if transcript.status == "error":
+        raise RuntimeError(f"Transcription failed: {transcript.error}")
+    return(transcript.text)
+
+
+# print(convertor(r"recording/test.wav"))
--- code/main.py
+++ code/main.py
@@ -1,8 +1,9 @@
 import sounddevice as sd
 import numpy as np 
-import  soundfile as sf
+import soundfile as sf
 import datetime as dt   
-
+import SPT_api as spai
+import lmstudio as lms
 
 sd.default.samplerate = 48000
 sd.default.blocksize = 1024
@@ -18,13 +19,41 @@ def playingsound(arr, sampling_rate = sd.default.samplerate):
      sd.play(arr, sampling_rate)
      sd.wait()
 
-with sd.InputStream(
-    callback=audio_callback
-):
-    input("press enter to stop\n")
+def record():
+    with sd.InputStream(
+        callback=audio_callback
+    ):
+        input("press enter to stop\n")
 
 
-full_audio = np.concatenate(rec_list, axis=0)
-playingsound(full_audio)
-time = dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-sf.write(f"recording/{time}.wav", full_audio, sd.default.samplerate)
+def corrector(text):
+    model = lms.llm("gemma-3-4b-it")
+    response = model.respond(text)
+    return response
+    
+
+while True:
+    try :
+        rec_list.clear()
+        record()
+        full_audio = np.concatenate(rec_list, axis=0)
+        playingsound(full_audio) 
+        ask = str(input("Enter y/n :: "))
+        if ask.lower() == "y":
+            sf.write(f"recording/test.wav", full_audio, sd.default.samplerate)
+            break  
+        else :
+            continue
+    except:
+        continue
+
+
+
+
+# time = dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")y'
+# sf.write(f"recording/{time}.wav", full_audio, sd.default.samplerate) 
+
+
+
+text = (spai.convertor(r"recording/test.wav"))
+print(corrector(f'give me grammatically correct version of this "{text}" sentence , just give me correct version'))
