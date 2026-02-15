8612ec6794cce3f62ece25d6b0e028b1e90779d0
Manan_Uppal <muppal004@gmail.com>
2026-02-15 17:44:58 +0530

    version 1_correct

--- code/SPT_api.py
+++ code/SPT_api.py
@@ -1,6 +1,6 @@
 import assemblyai as aai
 
-aai.settings.api_key = "bf34c4cf41a64aea8782324a682120d0"
+# aai.settings.api_key = "bf34c4cf41a64aea8782324a682120d0"
 config = aai.TranscriptionConfig(speech_models=["universal-3-pro", "universal-2"], language_detection=True)
 
 
